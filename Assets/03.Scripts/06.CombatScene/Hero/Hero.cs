c0fe091748dd788345f278326bd4a105a0c0fa3c
KkabyDol <rlaskarb0321@gmail.com>
2023-01-11 18:19:47 +0900

    몬스터 포메이션, 컴벳씬매니저 생성
    몬스터들이 보스들과 있을때와 없을때관련 진형의 틀을 만들음
    컴뱃씬매니저관련 프리팹을 만들어서 게임매니저와 차별점을 둠

---
+++ Assets/03.Scripts/06.CombatScene/Hero/Hero.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Hero : MonoBehaviour
+{
+    public float _health;
+    public float _atkPower;
+    public int _spiritCount;
+}
