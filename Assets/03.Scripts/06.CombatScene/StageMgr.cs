89ee4096830abea43dd2718f089c1877fbac63c6
KkabyDol <rlaskarb0321@gmail.com>
2023-02-17 17:31:38 +0900

    Stage전환하는 Fade효과 구현
    Canvas하위에 panel을 추가
    Panel은 StageAlarmTxt를 가지고있고
    기본적으로 둘 다 비활성화인 상태이며
    In Stage Clear일 경우에 활성화하고 본인의 일을 한다.

--- Assets/03.Scripts/06.CombatScene/StageMgr.cs
+++ Assets/03.Scripts/06.CombatScene/StageMgr.cs
@@ -12,19 +12,13 @@ public class StageMgr : MonoBehaviour, IGameFlow
     public bool _isBossStageClear;
     public List<bool> _isStageClear;
 
-    [Header("=== UI ===")]
-    public Image _stageBackGroundImg;
-    public Image _fadeInOutPanel;
-    public Text _stageAlarmTxt;
+    private CombatUI _ui;
     private CombatSceneMgr _combatMgr;
-
-    Animator _anim;
-    private int _hashToUp = Animator.StringToHash("isTimeToUp");
 
     private void Awake()
     {
+        _ui = GetComponent<CombatUI>();
         _combatMgr = GetComponent<CombatSceneMgr>();
-        _anim = _stageAlarmTxt.GetComponent<Animator>();
     }
 
     public void DoGameFlowAction()
@@ -35,23 +29,20 @@ public class StageMgr : MonoBehaviour, IGameFlow
 
     IEnumerator DoStageClearAction()
     {
-        _stageAlarmTxt.text = $"STAGE 1\n{_currLevel} / {_maxLevelValue}";
         HeroTeamMgr heroTeamMgr = _combatMgr._heroGroup.GetComponent<HeroTeamMgr>();
         heroTeamMgr.LooseHeroDmg();
 
         while (GameManager._instance._gameFlow == eGameFlow.InStageClear)
         {
-            // ui들은 나중에 새로운 스크립트로 분리
-            // 패널로 Fade in&out 효과를 주고 스테이지 이동때 필요한 작업을 함
-            yield return new WaitForSeconds(2.5f);
+            yield return StartCoroutine(_ui.StartFadeOut());
             _combatMgr.GoToNextStage();
-            GameManager._instance._gameFlow = eGameFlow.Idle;
+            _ui.SetRountTxt(_currLevel, _maxLevelValue);
+            StartCoroutine(_ui.ShowTxtFade());
 
-            /* 패널
-             * 패널은 평상시엔 activeSelf = false이고, InStageClear일때 true로 놓음, 아이템블럭과 유사한 방식으로 애님재생
-             * 패널로 Fade in&out 효과를 줄 때 stage 알림 ui 애니메이션도 같이재생, 가장 어두운 시점에 event를 달아놓는다
-             * 해당 event때 스테이지 이동 코드작업
-             */
+            yield return _ui._ws;
+            yield return StartCoroutine(_ui.StartFadeIn());
+
+            GameManager._instance._gameFlow = eGameFlow.Idle;
             yield return null;
         }
     }
---
+++ Assets/03.Scripts/06.CombatScene/UI/CombatUI.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombatUI : MonoBehaviour
+{
+    public Image _stageBackGroundImg;
+    public Image _fadeInOutPanel;
+    public Text _stageAlarmTxt;
+
+    public float _fadeValue;
+    public float _delayValue;
+    public WaitForSeconds _ws;
+
+    private void Start()
+    {
+        _ws = new WaitForSeconds(_delayValue);
+    }
+
+    // »≠∏È¿ª æÓµ”∞‘ «ÿ¡‹
+    public IEnumerator StartFadeOut()
+    {
+        if (!_fadeInOutPanel.gameObject.activeSelf)
+            _fadeInOutPanel.gameObject.SetActive(true);
+
+        Color color = _fadeInOutPanel.color;
+        while (color.a < 1.0f)
+        {
+            color.a += _fadeValue * Time.deltaTime;
+            _fadeInOutPanel.color = color;
+            yield return null;
+        }
+    }
+
+    // »≠∏È¿ª π‡∞‘ «ÿ¡‹
+    public IEnumerator StartFadeIn()
+    {
+        _stageAlarmTxt.gameObject.SetActive(false);
+
+        Color color = _fadeInOutPanel.color;
+        while (color.a > 0.0f)
+        {
+            color.a -= _fadeValue * Time.deltaTime;
+            _fadeInOutPanel.color = color;
+            yield return null;
+        }
+        _fadeInOutPanel.gameObject.SetActive(false);
+    }
+
+    public void SetRountTxt(int currRound, int maxRound)
+    {
+        if (!_stageAlarmTxt.gameObject.activeSelf)
+            _stageAlarmTxt.gameObject.SetActive(true);
+
+        if (currRound == maxRound)
+            _stageAlarmTxt.text = $"STAGE 1\n LAST ROUND";
+        else
+            _stageAlarmTxt.text = $"STAGE 1\n {currRound}/{maxRound}";
+    }
+
+    public IEnumerator ShowTxtFade()
+    {
+        if (!_stageAlarmTxt.gameObject.activeSelf)
+            _stageAlarmTxt.gameObject.SetActive(true);
+
+        Color color = _stageAlarmTxt.color;
+        while (color.a < 1.0f)
+        {
+            color.a += (2.0f * _fadeValue) * Time.deltaTime;
+            _stageAlarmTxt.color = color;
+            yield return null;
+        }
+    }
+}
